c69b2fb08d190ef7a4fa59235646e72868fa29ab
Yonatan Ginsburg <yginsburg26@gmail.com>
2019-03-24 22:11:38 -0400

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * This is literally to just test if I can get jitpack to host javadocs. This repo will go down
+ */
+public class Hello {
+
+  /**
+   * Says the passed message
+   * @param msg The message to say
+   */
+  void say(String msg) {
+    System.out.println(msg);
+  }
+}
